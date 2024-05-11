dc5664cb135124219ceedc1ea58135834f372964
dweller4085 <immbirj@gmail.com>
2024-05-11 10:34:33 +0700

    added a queue stump

---
+++ include/mkb/containers/queue.hh
@@ -0,0 +1,9 @@
+#ifndef __mkb_queue__
+#define __mkb_queue__
+
+#include <queue>
+
+// [mkb] (11 may '24) one day I'll write my own
+template<typename t_> using t_queue = std::queue<t_>;
+
+#endif // __mkb_queue__
